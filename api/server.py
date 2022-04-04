15d53a4c15856c38404bf0768a88499e94dbd94c
svalencia014 <sv.samuel.valencia014@gmail.com>
2022-04-04 18:21:29 -0400

    Last commit before switch to JS:

--- api/server.py
+++ api/server.py
@@ -1,10 +1,20 @@
 from flask import Flask, request, jsonify
+from time import sleep
+
+#import firebase shit
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
-app.run(host='0.0.0.0', port="9000");
+app.run(host='0.0.0.0', port="9000")
 print("listening on port 9000")
 
-messages = [{}]
+messages = []
+
+f = open("messages.json", "w")
+
+#Use application defualt crednetials
+
 
 @app.post("/messages")
 def add_message():
@@ -15,3 +25,6 @@ def add_message():
         return content, 201
     return {"error": "Request must be JSON"}, 415
         
+def sync_messages(f, messages):
+    sleep(60)
+    f.read()
