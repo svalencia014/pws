8c0eb9c82869d3a6490c02e241b186d3bc9a56c6
Samuel Valencia <sv.samuel.valencia014@gmail.com>
2022-04-05 21:12:04 -0400

    update

---
+++ api/routes/index.js
@@ -0,0 +1,61 @@
+//Import express shit
+const express = require("express");
+const router = express.Router();
+
+//Import Google shit
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+//Initialize Firebase
+initializeApp();
+const db = getFirestore();
+
+//Routes (for reference)
+    //Get
+    //Post
+    //Delete
+
+    router.get('/messages', (req, res) => {
+        //Get messages
+    });
+
+    router.get('/messages/id/', (req, res) => {
+        //Get message by id
+        const search = req.body;
+        const id = search.id;
+        const docRef = db.collection('messages').doc(`${id}`);
+        const doc = docRef.get();
+        console.log(doc);
+
+        res.send(doc);
+    });
+    
+    router.post('/messages', (req, res) => {
+        const message = req.body;
+        const name = message.name;
+        const email = message.email;
+        const body = message.body;
+        const id = Math.floor(100000 + Math.random() * 900000);
+        //post it to firestore
+        const docRef = db.collection('messages').doc(`${id}`);
+        docRef.set({ 
+            "name": name,
+            "email": email,
+            "body": body,
+            "id": id
+         });
+
+        console.log(`Added ${id} to the database`);
+
+        return res.sendStatus(201);
+    });
+    
+    router.delete('/messages/delete', (req, res) => {
+        //Delete messages
+        const target = req.body;
+        const id = target.id;
+        const docRef = db.collection('messages').doc(`${id}`).delete();
+        return res.sendStatus(202);
+    });
+
+module.exports = router;
