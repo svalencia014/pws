474cce1d4d99355ec33470c94a6d350da1dd7dc2
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-03-05 13:24:20 -0500

    Update

--- src/ids/scripts/wx.js
+++ src/ids/scripts/wx.js
@@ -1,4 +1,12 @@
-addEventListener("load", () => {
-    let airport = window.location.pathname.split("/").pop();
-    console.log(airport);
+addEventListener("load", async () => {
+    let airport = document.title.split(' ')[0].toLowerCase();
+    let request = new XMLHttpRequest()
+    request.open("GET", "https://testrepl.svalencia14.repl.co/metar/" + airport);
+    request.send();
+    console.log(request.responseText);
+    /*let metarDiv = document.getElementById('metar');
+    metarDiv.innerHTML = `<p>${request.response}</p>`;
+    if (metarDiv.innerHTML == "<p></p>") {
+        metarDiv.innerHTML = "<p>Failed to fetch</p>";
+    }*/
 });
