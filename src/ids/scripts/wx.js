ba6abbd69543f660636ae1622ee0f8d7ce263c38
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-03-06 07:38:19 -0500

    runway js works

--- src/ids/scripts/wx.js
+++ src/ids/scripts/wx.js
@@ -1,12 +1,12 @@
 addEventListener("load", async () => {
     let airport = document.title.split(' ')[0].toLowerCase();
     let request = new XMLHttpRequest()
-    request.open("GET", "https://testrepl.svalencia14.repl.co/metar/" + airport);
+    request.open("GET", "https://testrepl.svalencia14.repl.co/metar/" + airport, false);
     request.send();
     console.log(request.responseText);
-    /*let metarDiv = document.getElementById('metar');
+    let metarDiv = document.getElementById('metar');
     metarDiv.innerHTML = `<p>${request.response}</p>`;
     if (metarDiv.innerHTML == "<p></p>") {
         metarDiv.innerHTML = "<p>Failed to fetch</p>";
-    }*/
+    }
 });
