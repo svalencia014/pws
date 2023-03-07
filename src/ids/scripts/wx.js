3983a2779654b827d9ee7592652f698e250cf86c
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-03-06 20:08:55 -0500

    Runway selection works

--- src/ids/scripts/wx.js
+++ src/ids/scripts/wx.js
@@ -3,7 +3,6 @@ addEventListener("load", async () => {
     let request = new XMLHttpRequest()
     request.open("GET", "https://testrepl.svalencia14.repl.co/metar/" + airport, false);
     request.send();
-    console.log(request.responseText);
     let metarDiv = document.getElementById('metar');
     metarDiv.innerHTML = `<p>${request.response}</p>`;
     if (metarDiv.innerHTML == "<p></p>") {
@@ -22,9 +21,29 @@ addEventListener("load", async () => {
     for (i in runwayList) {
         headingList[i] = runwayList[i].replace("L","");
         headingList[i] = headingList[i].replace("R","");
+        headingList[i] = headingList[i] + "0";
     }
 
-    console.log(runwayList);
-    console.log(headingList);
+    let metar = document.getElementById("metar").innerText.split(" ");
+    let winds = [
+        metar[2].slice(0, 3),
+        metar[2].slice(3)
+    ]
 
+    if (winds[1].includes("G")) {
+        //High winds
+    } 
+    else if (winds[1].slice(0, 2) < 10 || winds[1].slice(0, 2) == "VRB")  {
+        document.getElementById("calmwind").innerHTML = `${document.getElementById("calmwind").innerText} - Recommended Operations`;
+    }
+    else {
+        i = 0;
+        let windDifference = [];
+        for (i in headingList) {
+            windDifference[i] = parseInt(headingList[i]) - winds[0];
+            windDifference[i] = Math.abs(windDifference[i]);
+        }
+        let bestRunway = headingList[windDifference.indexOf(Math.min(...windDifference))];
+        document.querySelector(`.r${bestRunway.toString()}`).innerHTML = document.querySelector(`.r${bestRunway.toString()}`).innerText + ` - Recommended Runway- ${runwayList[windDifference.indexOf(Math.min(...windDifference))]}`;
+    };
 });
