998789214f67bda4b540a4d15ad3232e81234456
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-07-05 20:05:54 -0400

    update some shit

--- src/routes/social/+page.svelte
+++ src/routes/social/+page.svelte
@@ -1,6 +1,9 @@
 <script>
   import Navbar from "../../components/Navbar.svelte";
   import Footer from "../../components/Footer.svelte";
+  import Fa from 'svelte-fa/src/fa.svelte';
+  import { faTwitter, faTwitch } from '@fortawesome/free-brands-svg-icons';
+  import '../../app.css'
 </script>
 <svelte:head>
   <style>
@@ -9,44 +12,24 @@
   </style>
   <title>Samuel V.</title>
 </svelte:head>
-<style>
-  :global(main) {
-    color: #f5821f;
-  }
-  :global(body) {
-    background: #4d2c2b;
-    margin: 0;
-  }
-  :global(.topnav) {
-    overflow: hidden;
-    background-color: #3a3434;
-    font-family: "Bebas Neue", cursive;
-  }
-  :global(.topnav .icon) {
-    display: none;
-  }
-  :global(.topnav a ) {
-    float: left;
-    color: #f5821f;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 20px
-  }
-  :global(.topnav a:hover, .card:hover) {
-    background-color: #ddd;
-    color: black;
-  }
-  :global(.topnav a.active) {
-    background-color: #66746f;
-    color: white;
-  }
-</style>
+
 <div class="topnav" id="myTopnav">
   <Navbar />
 </div>
 <main>
-  <p>Socials</p>
+  <p>Here are links to my socials</p>
+  <div class="socialRow">
+    <div class="socialElement">
+      <a href="https://twitter.com/bifocalcanvas" target="_blank">
+        <Fa icon={faTwitter} class="fa-brands fa-twitter fa-3x"/>
+      </a>
+    </div>
+    <div class="socialElement">
+      <a href="https://twitch.tv/bifocalcanvas77" target="_blank">
+        <Fa icon={faTwitch} class="fa-brands fa-twitch fa-3x"/>
+      </a>
+    </div>
+  </div>
 </main>
 <hr>
 <footer>
