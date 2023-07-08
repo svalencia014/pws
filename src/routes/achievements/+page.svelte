c4870f01bfc9f79448c50bec90f7430f244a43d4
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-07-07 23:22:05 -0400

    Working hard or hardly working

---
+++ src/routes/achievements/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import '../../app.css';
+  import Navbar from '../../components/Navbar.svelte';
+  import Footer from '../../components/Footer.svelte';
+</script>
+<div class="topnav" id="myTopnav">
+  <Navbar />
+</div>
+<main>
+  <h1>Achievements</h1>
+  <p>Coming soon...</p>
+</main>
+<hr>
+<footer>
+  <Footer />
+</footer>
