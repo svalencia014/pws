4b4585b8d25b48364cb84ed5a6ab617cea05422d
Samuel Valencia <68522351+svalencia014@users.noreply.github.com>
2024-03-11 22:06:01 -0400

    Update +page.svelte
    fixe things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,10 +19,10 @@
   <div class="links">
     <div class="banners">
       <div class="nocard">
-        <img src="https://vercel.svalencia.me/api?username=svalencia014&show_icons=true&theme=dracula" alt="GitHub Stats"/>
+        <img src="https://github-readme-stats-svalencia014.vercel.app/api?username=svalencia014&show_icons=true&theme=dracula" alt="GitHub Stats"/>
       </div>
       <div class="nocard">
-        <img src="https://vercel.svalencia.me/api/top-langs?username=svalencia014&theme=dracula&layout=compact" alt="Used Languages"/>
+        <img src="https://github-readme-stats-svalencia014.vercel.app/api/top-langs?username=svalencia014&theme=dracula&layout=compact" alt="Used Languages"/>
       </div>
       <div class="nocard">
         <img src="https://github-readme-streak-stats.herokuapp.com?user=svalencia014&theme=dracula" alt="GitHub streak stats"/>
